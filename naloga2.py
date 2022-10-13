8f60d3cc035c679286c898355be992a36afe3a94
mmzezi <mtcmarkizeti@gmail.com>
2022-10-13 18:56:05 +0200

    Domača naloga 1

---
+++ naloga2.py
@@ -0,0 +1,5 @@
+a = input("Vnesite prvo število: ")
+b = input("Vnesite drugo število: ")
+
+c = int(a) + int(b)
+print(c)
---
+++ naloga3.py
@@ -0,0 +1,7 @@
+x1 = input("Vnesite prvo število: ")
+x2 = input("Vnesite drugo število: ")
+x3 = input("Vnesite tretje število: ")
+
+if int(x2) == int(x1) and int(x3) <= int(x1): #python nima && rabiš dati AND
+    print("Druga vrednost je enaka prvi in tretja je manjša ali enaka prvi")
+
